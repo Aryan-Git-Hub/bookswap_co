95d788b71b623f43fd4646bcbf207bea12da2382
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2024-12-25 12:57:50 +0530

    login required for post ad and checkout

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from books.models import Book
 from books.forms import BookForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -9,10 +10,12 @@ def index(request):
     return render(request, 'books/index.html', {"all_books":all_books})
 
 
+@login_required
 def checkout(request):
     return render(request, 'books/checkout.html')
 
 
+@login_required
 def post_ad(request):
     fm = BookForm()
     if request.method=="POST":
