d791bbea6a2e64f0ca13ff9e05976d2b9ec14961
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2024-12-27 11:20:49 +0530

    resizing book image

--- books/models.py
+++ books/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from accounts.models import CustomUser
+# to change dimentions of image
+from PIL import Image
+
+
+def resize_img(img_path, output_size):
+    img = Image.open(img_path)
+    img.thumbnail(output_size)
+    background = Image.new('RGB', output_size, (255, 255, 255))  # Transparent background
+    offset = ((output_size[0] - img.size[0]) // 2, (output_size[1] - img.size[1]) // 2)
+    background.paste(img, offset)
+    img = background
+    img.save(img_path, format='JPEG', quality=95)
+
 
 # Create your models here.
 class Book(models.Model):
@@ -14,3 +27,10 @@ class Book(models.Model):
     category = models.CharField(max_length=50, blank=True)
     pages = models.IntegerField(default=0, null=True)
     photo = models.ImageField(upload_to='books/', blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.photo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 330) # (width, height=1.1*width)
+            resize_img(self.photo.path, output_size)
