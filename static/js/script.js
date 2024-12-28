2f6b4dbd84188b14eb9de19dc248d3f65f123d61
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2024-12-28 23:27:28 +0530

    fixing search button bug

--- static/js/script.js
+++ static/js/script.js
@@ -11,20 +11,36 @@ for (let index = 0; index < logout.length; index++) {
 
 
 // for search_results
-const search_for = document.getElementById("search_for");
-const search_btn = document.getElementById("search_btn");
-const search_logo = document.getElementById("search_logo");
+const search_for_1 = document.getElementById("search_for_1");
+const search_for_2 = document.getElementById("search_for_2");
+let search_btn = document.getElementsByClassName("search_btn");
+let search_logo = document.getElementsByClassName("search_logo");
+
+
 
 function disable_search() {
-  if (search_for.value=="" || search_for.value==null) {
-      search_btn.disabled = true;
-      search_logo.classList.remove("search_logo_transform");
-      search_logo.title = "Please type something to Search...";
+  if ((search_for_1.value=="" || search_for_1.value==null) && (search_for_2.value=="" || search_for_2==null)) {
+    for (let j = 0; j < search_btn.length; j++) {
+      const element = search_btn[j];
+      element.disabled = true;
+    }
+      for (let i = 0; i < search_logo.length; i++) {
+        const element = search_logo[i];
+        element.classList.remove("search_logo_transform");
+        element.title = "Please type something to Search...";
+      }
   } else {
-      search_btn.disabled = false;
-      search_logo.classList.add("search_logo_transform");
-      search_logo.removeAttribute("title");
+    for (let j = 0; j < search_btn.length; j++) {
+      const element = search_btn[j];
+      element.disabled = false;
+    }
+      for (let i = 0; i < search_logo.length; i++) {
+        const element = search_logo[i];
+        element.classList.add("search_logo_transform");
+        element.removeAttribute("title");
+      }
   }
 }
 disable_search();
-search_for.addEventListener('input', disable_search);
+search_for_1.addEventListener('input', disable_search);
+search_for_2.addEventListener('input', disable_search);
