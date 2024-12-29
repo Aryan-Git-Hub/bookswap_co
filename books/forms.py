ec1cfbc1a28c7883fc3f11007386c6ad5ee72d60
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2024-12-29 14:11:25 +0530

    Category choices option added in post_ad

--- books/forms.py
+++ books/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from books.models import Book, ImageModel
 from accounts.models import CustomUser
+# for passing label with html tags
+from django.utils.safestring import mark_safe
 
 STATE_CHOICES = [
   [None, "---Select State---"],
@@ -44,19 +46,32 @@ STATE_CHOICES = [
   ["West Bengal", "West Bengal"]
 ]
 
+CATEGORY_CHOICES = (
+   (None, "---Select State---"),
+   ("B.Tech.", "B.Tech."),
+   ("M.Tech.", "M.Tech."),
+   ("MBBS", "MBBS"),
+   ("JEE", "JEE"),
+   ("NEET PG", "NEET PG"),
+   ("NEET UG", "NEET UG"),
+   ("NCERT", "NCERT"),
+   ("Education", "Education"),
+   ("Other", "Other"),
+)
 
+asterisk_css = '<span style="color: red; font-weight:bolder;">*</span>'
 class BookForm(forms.ModelForm):
-    book_name = forms.CharField(label='Book Name*', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    author = forms.CharField(label='Author*', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    book_name = forms.CharField(label=mark_safe('Book Name'+asterisk_css), max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    author = forms.CharField(label=mark_safe('Author'+asterisk_css), max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     desc = forms.CharField(label='Description', max_length=300, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
     edition = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
-    publication = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
-    category = forms.CharField(label='Category*', max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    pages = forms.IntegerField(min_value=0, label='Pages*', widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    publication = forms.CharField(label=mark_safe('Publication'+asterisk_css), max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    category = forms.ChoiceField(choices=CATEGORY_CHOICES, label=mark_safe('Category'+asterisk_css), widget=forms.Select(attrs={'class': 'form-control'}))
+    pages = forms.IntegerField(label=mark_safe('Pages'+asterisk_css), min_value=0, widget=forms.NumberInput(attrs={'class': 'form-control'}))
     seller = forms.ModelChoiceField(queryset=CustomUser.objects.all(), widget=forms.HiddenInput)
-    state = forms.ChoiceField(label='State*', choices=STATE_CHOICES, widget=forms.Select(attrs={'class': 'form-control', 'id':'state_id'}))
-    city = forms.ChoiceField(label='City*', choices=[], widget=forms.Select(attrs={'class': 'form-control', 'id':'city_id'}))
-    price = forms.IntegerField(label='Price*', min_value=10, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    state = forms.ChoiceField(label=mark_safe('State'+asterisk_css), choices=STATE_CHOICES, widget=forms.Select(attrs={'class': 'form-control', 'id':'state_id'}))
+    city = forms.ChoiceField(label=mark_safe('City'+asterisk_css), choices=[], widget=forms.Select(attrs={'class': 'form-control', 'id':'city_id'}))
+    price = forms.IntegerField(label=mark_safe('Price'+asterisk_css), min_value=10, widget=forms.NumberInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Book
