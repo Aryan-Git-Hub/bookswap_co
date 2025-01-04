d14b272d4cd8789ea0b441a82ab4c024e193b3ba
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2025-01-04 16:10:01 +0530

    forgot or change password feature added

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.auth_login, name='login'),
     path('logout/', views.auth_logout, name='logout'),
+    path('change_pass/', views.change_password, name="change_pass"),
     path('otp/', views.otp, name='otp'),
     path('profile/', views.profile, name='profile'),
     path('addresses/', views.user_saved_addresses, name='user_saved_addresses'),
