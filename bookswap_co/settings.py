67ed3cf0387ffbc927e19dc40a8ead18c81db873
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2025-01-13 00:37:54 +0530

    turning DEBUG = False for hosting website at pythonanywhere

--- bookswap_co/settings.py
+++ bookswap_co/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$k+%o70d3hk)6l68*rvnzz5a%+)u&2p78tiroij^^u$g7ta5e+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
