bea60bf084e17ccaa9723ea23923749bf034b97d
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2025-01-14 15:25:25 +0530

    enabling DEBUG=True for developing stage

--- bookswap_co/settings.py
+++ bookswap_co/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$k+%o70d3hk)6l68*rvnzz5a%+)u&2p78tiroij^^u$g7ta5e+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
